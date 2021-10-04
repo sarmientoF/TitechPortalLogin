26b5bdf3fad886699cb96857d6f2ab16634b975f
Fernando <fsarmientod@uni.pe>
2021-10-04 14:55:22 +0900

    Initial Commit

---
+++ DesktopApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DesktopApp
+//
+//  Created by Fernando Gabriel Sarmiento Diaz on 2021/10/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DesktopApp/DesktopAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesktopAppApp.swift
+//  DesktopApp
+//
+//  Created by Fernando Gabriel Sarmiento Diaz on 2021/10/04.
+//
+
+import SwiftUI
+
+@main
+struct DesktopAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
